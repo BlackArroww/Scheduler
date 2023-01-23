34e23e931f5003fa8764e21b0d349e8aa30359f8
BlackArroww <78801797+BlackArroww@users.noreply.github.com>
2023-01-23 21:15:38 +0100

    Small Change in View

--- Scheduler/src/view_scheduler.java
+++ Scheduler/src/view_scheduler.java
@@ -14,6 +14,7 @@ import javax.swing.JTextField;
 import javax.swing.JList;
 import java.awt.CardLayout;
 import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 public class view_scheduler extends JFrame {
 
@@ -22,6 +23,7 @@ public class view_scheduler extends JFrame {
 	private JTextField priority_fld;
 	private JTextField work_field;
 	private JTextField wait_fld;
+	private JTextField work_fld;
 
 	/**
 	 * Launch the application.
@@ -65,7 +67,7 @@ public class view_scheduler extends JFrame {
 		
 		JLabel prozess_lbl = new JLabel("Prozesse :");
 		prozess_lbl.setFont(new Font("Tahoma", Font.BOLD, 23));
-		prozess_lbl.setBounds(10, 11, 172, 31);
+		prozess_lbl.setBounds(10, 23, 172, 31);
 		scheduler.add(prozess_lbl);
 		
 		JButton apl_btn = new JButton("Alle Prozesse löschen");
@@ -74,7 +76,7 @@ public class view_scheduler extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		apl_btn.setBounds(141, 11, 193, 31);
+		apl_btn.setBounds(141, 23, 193, 31);
 		scheduler.add(apl_btn);
 		
 		JButton hzf_btn = new JButton("Hinzufügen");
@@ -109,10 +111,10 @@ public class view_scheduler extends JFrame {
 		priority_fld.setBounds(47, 258, 287, 43);
 		scheduler.add(priority_fld);
 		
-		work_field = new JTextField();
-		work_field.setColumns(10);
-		work_field.setBounds(47, 378, 287, 43);
-		scheduler.add(work_field);
+		work_fld = new JTextField();
+		work_fld.setColumns(10);
+		work_fld.setBounds(47, 378, 287, 43);
+		scheduler.add(work_fld);
 		
 		wait_fld = new JTextField();
 		wait_fld.setColumns(10);
@@ -141,12 +143,15 @@ public class view_scheduler extends JFrame {
 		
 		JLabel scheduler_lbl = new JLabel("Scheduler :");
 		scheduler_lbl.setFont(new Font("Tahoma", Font.BOLD, 23));
-		scheduler_lbl.setBounds(874, 11, 172, 31);
+		scheduler_lbl.setBounds(874, 23, 172, 31);
 		scheduler.add(scheduler_lbl);
 		
 		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(1036, 11, 193, 31);
+		comboBox.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"RoundRobin", "FirstComeFirstServe"}));
+		comboBox.setBounds(1036, 23, 193, 31);
 		scheduler.add(comboBox);
+		
 		
 		JPanel prozesse = new JPanel();
 		tabbedPane.addTab("Prozesse", null, prozesse, null);
